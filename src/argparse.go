33dafb86f94aa4bc0217ce6b508af6d7d0e10f4c
olaf michaelis <o.mic@web.de>
2023-04-19 20:43:56 +0200

    Make linter happy

--- src/argparse.go
+++ src/argparse.go
@@ -18,20 +18,20 @@ var (
 )
 
 var CLI struct {
-	Command      []string `help:"command to run, flags always have to be in front" arg optional passthrough`
-	Folder       string   `help:"folder to watch, default is current dir" optional short:f default:${curdir}`
-	Regex        string   `help:"regex scheme, only consider files that match" optional short:r default:.*`
-	Spectate     bool     `help:"spectate mode, do not run command, just watch and print file system changes" short:s`
-	Interval     int32    `help:"watch interval, recheck for changes in seconds" default:1 short:i`
-	Pause        int32    `help:"pause before running the command (seconds)" default:0 short:p`
+	Command      []string `help:"command to run, flags always have to be in front" arg:"" optional:"" passthrough:""`
+	Folder       string   `help:"folder to watch, default is current dir" optional:"" short:"f" default:"${curdir}"`
+	Regex        string   `help:"regex scheme, only consider files that match" optional:"" short:"r" default:".*"`
+	Spectate     bool     `help:"spectate mode, do not run command, just watch and print file system changes" short:"s"`
+	Interval     int32    `help:"watch interval, recheck for changes in seconds" default:"1" short:"i"`
+	Pause        int32    `help:"pause before running the command (seconds)" default:"0" short:"p"`
 	RunInitially bool     `help:"run command initially, normal behaviour is to run on first change of files"`
-	KeepOutput   bool     `help:"keep output, do not clear screen when running command" short:k`
-	LogFile      string   `help:"log file" short:l default:/dev/stdout`
-	LogLevel     string   `help:"log level" short:e default:info enum:"debug,info,error"`
-	LogNoColors  bool     `help:"disable output colours, print plain text" short:c`
-	LogJSON      bool     `help:"enable json log, instead of text one" short:j`
-	PrintVars    bool     `help:"print a list of available variables" short:n`
-	VersionFlag  bool     `help:"display version" short:V`
+	KeepOutput   bool     `help:"keep output, do not clear screen when running command" short:"k"`
+	LogFile      string   `help:"log file" short:"l" default:"/dev/stdout"`
+	LogLevel     string   `help:"log level" short:"e" default:"info" enum:"debug,info,error"`
+	LogNoColors  bool     `help:"disable output colours, print plain text" short:"c"`
+	LogJSON      bool     `help:"enable json log, instead of text one" short:"j"`
+	PrintVars    bool     `help:"print a list of available variables" short:"n"`
+	VersionFlag  bool     `help:"display version" short:"V"`
 }
 
 func parseArgs() {
